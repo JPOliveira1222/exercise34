3923e2423def1a2a7aa711b14fc6372b6ec0a882
JP Oliveira <joaopedro.costa.oliveira@gmail.com>
2021-10-03 18:52:46 -0400

    pseudocode for 34

---
+++ src/main/java/exercise34/solution34.java
@@ -0,0 +1,26 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 34 Solution
+ *  Copyright 2021 Joao Pedro Oliveira
+ */
+
+package exercise34;
+
+import java.util.*;
+
+public class solution34 {
+
+    /*
+    Create a string containing the list of employees
+    print out the names of the employees
+
+    print "Enter an employee name to remove: "
+    removal - user input
+
+    remove the employee from the list
+    print out the remaining employees in the list
+
+     */
+
+    
+
+}
